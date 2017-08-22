304f328598d6aa4db4600b3f4fce4ad6a358d05e
Kuang Lin <kuanglin@linguangde-MacBook-Pro.local>
2017-08-22 19:49:19 +0800

    Feat: add facebook ad

--- app/src/main/java/com/example/kuanglin/tutortimerecord/ListActivity.java
+++ app/src/main/java/com/example/kuanglin/tutortimerecord/ListActivity.java
@@ -24,7 +24,7 @@ import android.widget.TextView;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.NativeExpressAdView;
+import com.facebook.FacebookSdk;
 
 import java.util.ArrayList;
 import java.util.Calendar;
